578c6adb9afa87a278243a9eb2bf8268e9468d2d
Jesse <jzhou242@optusnet.com.au>
2018-10-03 14:50:26 +0800

    Testing a commit with a local directory

---
+++ binary_struct_def.c
@@ -0,0 +1,10 @@
+#define NULL 0
+
+typedef struct node {
+
+	int val;
+	struct node * left;
+	struct node * right;
+	
+
+}point;
---
+++ binary_tree.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "/Users/JZ/Documents/Programming/binary_templates/binary_struct_def.c"
+
+point * initialise_list(void) {
+
+	point * start = NULL;
+	start = (point*)malloc(sizeof(point));
+	start->val = 0;
+	start->left = NULL;
+	start->right = NULL;
+	return start;
+	
+
+}
+
+void create_list(point * head) {
+
+	int i = 1;
+	point * current = head;
+	
+
+	for( ; i<11;i++) {
+		
+		current->val = i;
+		current->right = (point*)malloc(sizeof(point));
+		current = current->right;
+
+
+	}
+
+}
+
+void insert(point * test, int d) {
+	
+	point * current = test;
+	if(current->val == NULL){
+		current->val = d;
+
+	}
+
+	else{
+		if(d > current->val) {
+			if(current->right != NULL){
+				insert(current->right,d);
+
+			}
+
+			else{
+				current->right = (point*)malloc(sizeof(point));
+				current->right->val = d;
+
+			}	
+		}
+
+		else if(d < current->val) {
+			
+			if(current->left != NULL) {
+				insert(current->left,d);
+
+			}
+
+			else{
+				current->left = (point*)malloc(sizeof(point));
+				current->left->val = d;
+			}
+
+		}	
+
+	}
+
+}
+
+
+
+
+int main(void) {
+
+	point*head = initialise_list();
+	create_list(head);
+	printf("This is working\n");
+
+	return 0;
+}
